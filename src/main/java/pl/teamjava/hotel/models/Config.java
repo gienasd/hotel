30968e3da078d9fb1ab299bddc36abda6b9831fc
gienasd <30533388+gienasd@users.noreply.github.com>
2017-10-11 18:29:29 +0200

    Delete Config.java

--- src/main/java/pl/teamjava/hotel/models/Config.java
+++
@@ -1,6 +0,0 @@
-package pl.teamjava.hotel.models;
-
-public class Config {
-    public static final String APP_ID = "e3c57e70786aa5fc4948e0afe267e233";
-    public static final String APP_BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-}
